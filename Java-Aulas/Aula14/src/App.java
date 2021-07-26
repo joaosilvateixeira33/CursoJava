d594ea4fdedaa88cf44ad798e919953c9d10db1a
joao marcos <joaoteixeirasilva33@gmail.com>
2021-07-26 14:12:42 -0300

    atualização

---
+++ Java-Aulas/Aula14/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n1 = 89;
+        int n2 = 60;
+
+        System.out.println(n1 & n2);
+        System.out.println(n1 | n2);
+        System.out.println(n1 ^ n2);
+    }
+}
